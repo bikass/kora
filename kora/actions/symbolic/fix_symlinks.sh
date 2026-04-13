56f8ea153695384a17a6a464ba28e4ed35177798
bikass <spokas@protonmail.com>
2026-04-13 17:56:54 +0300

    symlink fixes

--- kora/actions/symbolic/fix_symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for link in *symbolic.svg; do
-  [ -L "$link" ] || continue
-  old=$(readlink "$link")
-  new=$(basename "$old")
-  rm "$link"
-  ln -sf "$new" "$link"
-  echo "Fixed $link: $old → $new"
-done
